37dd8fd36d4af6d4e57a503ff304457d88000f04
Simon Lee <simon@syntatic.deb>
2023-02-14 18:51:00 +0000

    Create marticle model for the blog

--- syntactic_web/blog/models.py
+++ syntactic_web/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Article(models.Model):
+    heading = models.CharField(max_length=200)
+    byline = models.CharField(max_length=200, null=True)
+    publish_date = models.DateField('date published', default=datetime.date)
+    active = models.BinaryField()
+    text = models.CharField(max_length=200)
