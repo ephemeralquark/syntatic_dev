6c19e150499a5ab7948859c4a23d74b9d73c3767
Simon Lee <simon@syntatic.deb>
2023-02-14 14:09:13 -0500

    Adding a simple model test to blog app

--- syntactic_web/blog/tests.py
+++ syntactic_web/blog/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Article
+
+
+class ArticleModelTests(TestCase):
+
+    def test_was_published_recently_with_future_article(self):
+        """
+        was_published_recently() returns False for articles whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_article = Article(publish_date = time)
+        self.assertIs(future_article.was_published_recently(), False)
